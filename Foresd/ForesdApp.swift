fe7dc3a87a5f6d789e0a6335bb6cc2f68c4df5ff
Dmitry Vorozhbicki <dima.worozbicki@gmail.com>
2022-01-03 11:13:25 +0100

    init project

---
+++ Foresd/ForesdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForesdApp.swift
+//  Foresd
+//
+//  Created by Dmitry Vorozhbicki on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForesdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
